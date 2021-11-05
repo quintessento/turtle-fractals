bb844f80b3264c35391ae902bd18a17687a0ee36
quintessento <46756767+quintessento@users.noreply.github.com>
2021-11-05 08:58:18 +0100

    Basic IFS shape
    Added three rectangles that form the basic starting shape which will be recursively transformed.

--- iterated_function_system.py
+++ iterated_function_system.py
@@ -1,4 +1,54 @@
 import turtle
+import math
+
+
+class Rectangle:
+    
+    def __init__(self, x, y, width, height, angle):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.angle = angle
+    
+    def apply_transform(self, translate_x, translate_y, scale_x, scale_y, rotate_z):
+        pass
+    
+    def draw(self, t):
+        t.penup()
+        
+        half_width = self.width * 0.5
+        half_height = self.height * 0.5
+        angle_rad = (self.angle + 90) * math.pi / 180.0
+        t.setpos(self.x - half_width * math.cos(angle_rad), self.y - half_height * math.sin(angle_rad))
+        t.setheading(self.angle + 90)
+        t.pendown()
+        t.forward(self.height)
+        t.right(90)
+        t.forward(self.width)
+        t.right(90)
+        t.forward(self.height)
+        t.right(90)
+        t.forward(self.width)
+        t.right(90)
+
+
+class Shape:
+    
+    def __init__(self):
+        self.rect1 = Rectangle(0, 0, 100, 100, 0)
+        self.rect2 = Rectangle(-70, 40, 120, 60, 15)
+        self.rect3 = Rectangle(60, 65, 90, 60, -20)
+        
+    def apply_transform(self, translate_x, translate_y, scale_x, scale_y, rotate_z):
+        self.rect1.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
+        self.rect2.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
+        self.rect3.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
+        
+    def draw(self, t):
+        self.rect1.draw(t)
+        self.rect2.draw(t)
+        self.rect3.draw(t)
 
 
 class IFS:
@@ -12,13 +62,11 @@ class IFS:
         ts.screensize(4000, 4000)
         ts.setup(width=1.0, height=1.0, startx=None, starty=None)
     
-    def go_to_point(self, a, b, c, d, e, f):
-        p = self.turtle.pos()
+    def generate(self):
+        shape = Shape()
+        shape.draw(self.turtle)
         
-        x = a * p[0] + b * p[1] + e
-        y = c * p[0] + d * p[1] + f
-        
-        self.turtle.goto(x, y)
+        turtle.mainloop()
 
 #                    #set 1     set 2     set 3     set 4
 #              #a     0.0100   -0.0100    0.4200    0.4200
@@ -30,5 +78,5 @@ class IFS:
 
 def run_example_1():
     ifs = IFS()
-    ifs.go_to_point(0.0100, 0.0000, 0.0000, 0.4500, 0.0000, 0.0000)
-    turtle.mainloop()
+    ifs.generate()
+
