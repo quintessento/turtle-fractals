973d86e50960b8a01f92b9a4d88074af585c524c
quintessento <46756767+quintessento@users.noreply.github.com>
2021-11-09 23:39:06 +0100

    Update iterated_function_system.py
    Fixed turtle tracing to 30 fps; something resembling a tree out of boxes is being drawn.

--- iterated_function_system.py
+++ iterated_function_system.py
@@ -1,5 +1,6 @@
 import turtle
 import math
+import os
 
 
 class Rectangle:
@@ -15,14 +16,16 @@ class Rectangle:
         pass
     
     def draw(self, t):
-        t.penup()
-        
+
         half_width = self.width * 0.5
         half_height = self.height * 0.5
         angle_rad = (self.angle + 90) * math.pi / 180.0
-        t.setpos(self.x - half_width * math.cos(angle_rad), self.y - half_height * math.sin(angle_rad))
+
+        t.penup()
+        t.setpos(self.x - half_width * math.cos(angle_rad), self.y - half_height * math.sin(angle_rad)) 
         t.setheading(self.angle + 90)
         t.pendown()
+
         t.forward(self.height)
         t.right(90)
         t.forward(self.width)
@@ -34,47 +37,88 @@ class Rectangle:
 
 
 class Shape:
-    
-    def __init__(self):
-        self.rect1 = Rectangle(0, 0, 100, 100, 0)
-        self.rect2 = Rectangle(-70, 40, 120, 60, 15)
-        self.rect3 = Rectangle(60, 65, 90, 60, -20)
-        
+
+    def __init__(self, x, y, width, height, angle):
+        self.children = []
+        self.rectangles = []
+
+        self.rectangles.append(Rectangle(x * 1.0, y * 1.0, width * 1.0, height * 1.0, angle * 1.0))
+        # self.rectangles.append(Rectangle(
+        #     x * 1.0 + width * -0.35, 
+        #     y * 1.0 + height * 0.45, 
+        #     width * 1.2, 
+        #     height * 0.6, 
+        #     angle + 360 * 0.04
+        # ))
+        # self.rectangles.append(Rectangle(
+        #     x * 1.0 + width * 0.25, 
+        #     y * 1.0 + height * 0.7,
+        #     width * 0.9, 
+        #     height * 0.6, 
+        #     angle - 360 * 0.05
+        # ))
+        self.rectangles.append(Rectangle(
+            x * 1.0 + width * -0.75, 
+            y * 1.0 + height * 0.45, 
+            width * 1.2, 
+            height * 0.6, 
+            angle + 360 * 0.04
+        ))
+        self.rectangles.append(Rectangle(
+            x * 1.0 + width * 0.5, 
+            y * 1.0 + height * 0.7,
+            width * 0.9, 
+            height * 0.6, 
+            angle - 360 * 0.05
+        ))
+
+    def expand(self, depth):
+        if depth > 0:
+            for r in self.rectangles:
+                self.children.append(Shape(r.x, r.y, r.width * 0.5, r.height * 0.5, r.angle))
+            self.rectangles.clear()
+
+            for s in self.children:
+                s.expand(depth - 1)
+
     def apply_transform(self, translate_x, translate_y, scale_x, scale_y, rotate_z):
-        self.rect1.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
-        self.rect2.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
-        self.rect3.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
+        pass
+        # self.rect1.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
+        # self.rect2.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
+        # self.rect3.apply_transform(translate_x, translate_y, scale_x, scale_y, rotate_z)
         
     def draw(self, t):
-        self.rect1.draw(t)
-        self.rect2.draw(t)
-        self.rect3.draw(t)
+        if not self.children:
+            for r in self.rectangles:
+                r.draw(t)
+
+        for s in self.children:
+            s.draw(t)
 
 
 class IFS:
     
     def __init__(self):
         self.turtle = turtle.Turtle()
-        self.turtle.speed(10)
+        self.turtle.hideturtle()
+        turtle.tracer(30, 0)
         self.turtle.setheading(90)
 
         ts = self.turtle.getscreen()
         ts.screensize(4000, 4000)
-        ts.setup(width=1.0, height=1.0, startx=None, starty=None)
+        
+        if os.name == 'nt':
+            ts.setup(width=0.99, height=0.99, startx=None, starty=None)
+        else:
+            ts.setup(width=1.00, height=1.00, startx=None, starty=None)
     
     def generate(self):
-        shape = Shape()
+        shape = Shape(0, 0, 400, 400, 0)
+        shape.expand(5)
         shape.draw(self.turtle)
         
         turtle.mainloop()
 
-#                    #set 1     set 2     set 3     set 4
-#              #a     0.0100   -0.0100    0.4200    0.4200
-#              #b     0.0000    0.0000   -0.4200    0.4200
-#              #c     0.0000    0.0000    0.4200   -0.4200
-#              #d     0.4500   -0.4500    0.4200    0.4200
-#              #e     0.0000    0.0000    0.0000    0.0000
-#              #f     0.0000    0.4000    0.4000    0.4000
 
 def run_example_1():
     ifs = IFS()
